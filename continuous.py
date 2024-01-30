cf81494d0ed91a811a4fe5f1754897c7d19dda63
Taylor O'Brien <toobrien@amazon.com>
2024-01-30 14:45:31 -0800

    .

--- continuous.py
+++ continuous.py
@@ -4,6 +4,15 @@ from    sys                     import  argv
 from    util                    import  get_continuous, r
 
 
+# python continuous.py RB:0:1 HO:0:-1
+# python continous.py  HO:0:1 HO:3:-2 HO:6:1:1
+
+
+def continuous_spread(symbols, terms, quantitites):
+
+    pass
+
+
 if __name__ == "__main__":
 
     pass
