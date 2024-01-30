06713c9c96f24e0c4e364d28a9dae3f9f14c34a7
Taylor O'Brien <toobrien@amazon.com>
2024-01-30 15:03:49 -0800

    .

--- continuous.py
+++ continuous.py
@@ -1,6 +1,7 @@
-from    math                    import  log
 import  plotly.graph_objects    as      go
+from    re                      import  compile
 from    sys                     import  argv
+from    typing                  import  List
 from    util                    import  get_continuous, r
 
 
@@ -8,11 +9,27 @@ from    util                    import  get_continuous, r
 # python continuous.py HO:0:1 HO:3:-2 HO:6:1:1
 
 
-def continuous_spread(symbols, terms, quantitites):
+def continuous_spread(
+    symbols:        List[str],
+    terms:          List[int],
+    quantitites:    List[int],
+    start:          str,
+    end:            str
+):
 
     pass
 
 
 if __name__ == "__main__":
+
+    pattern = compile("\d{4}-\d{2}-\d{2}")
+    dates   = [ date for date in argv if pattern.match(date) ]
+    start   = dates[0] if dates else "1900-01-01"
+    end     = dates[1] if len(dates) > 1 else "2100-01-01"
+    dfns    = [ arg.split(":") for arg in argv if ":" in arg ]
+    symbols = [ dfns[0] for dfn in dfns ]
+    terms   = [ int(dfns[1]) for dfn in dfns ]
+    qtys    = [ int(dfns[2]) for dfn in dfns ]
+    spread  = continuous_spread(symbols, terms, qtys, start, end)
 
     pass
