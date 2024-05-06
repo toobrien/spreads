71ddcc664b267c193a08e852d93df403ecc3e95e
Taylor O'Brien <toobrien@amazon.com>
2024-05-05 17:33:05 -0700

    .

--- lr_stat.py
+++ lr_stat.py
@@ -1,6 +1,7 @@
 from    math                    import  log
 from    numpy                   import  cumsum, mean, std
 import  plotly.graph_objects    as      go
+from    sys                     import  argv
 from    time                    import  time
 from    util                    import  get_continuous
 
@@ -8,9 +9,10 @@ from    util                    import  get_continuous
 if __name__ == "__main__":
 
     t0      = time()
-    es      = get_continuous("ES", "1900-01-01", "2100-01-01", 0)
-    y       = [ r[4] for r in es ]
-    text    = [ r[1] for r in es ]
+    symbol  = argv[1]
+    recs    = get_continuous(symbol, "1900-01-01", "2100-01-01", 0)
+    y       = [ r[4] for r in recs ]
+    text    = [ r[1] for r in recs ]
     logs    = [ log(y[i] / y[i - 1]) for i in range(1, len(y)) ]
     pcts    = [ y[i] / y[i - 1] - 1 for i in range(1, len(y)) ]
     y_      = cumsum(logs)
@@ -52,7 +54,7 @@ if __name__ == "__main__":
     print(f"avg (log_y):    {avg * 256:0.4f}")
     print(f"std (log_y):    {stdev * 16:0.4f}")
     print(f"days:           {len(logs)}")
-    print(f"years:          {len(logs) / 256:0.2f}")
+    print(f"years:          {len(logs) / 256:0.2f}\n")
     print(f"{time() - t0:0.1f}s")
 
     pass
